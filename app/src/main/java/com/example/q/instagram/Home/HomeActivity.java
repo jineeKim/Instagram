71175c704d59a94753d02ece1e4dd5768a35af1d
SungHwanKaist <sunghwankim@kaist.ac.kr>
2018-07-29 17:37:50 +0900

    login added

--- app/src/main/java/com/example/q/instagram/Home/HomeActivity.java
+++ app/src/main/java/com/example/q/instagram/Home/HomeActivity.java
@@ -1,6 +1,8 @@
 package com.example.q.instagram.Home;
 
 import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
@@ -9,8 +11,11 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.example.q.instagram.Login.LoginActivity;
 import com.example.q.instagram.R;
 import com.example.q.instagram.Utils.BottomNavigationViewHelper;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
 public class HomeActivity extends AppCompatActivity {
@@ -19,12 +24,15 @@ public class HomeActivity extends AppCompatActivity {
     private static final int ACTIVITY_NUM = 0;
     private Context mContext = HomeActivity.this;
 
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         Log.d(TAG,"onCreate: starting.");
 
+        setupFirevaseAuth();
         setupBottomNavigationView();
         setupViewPager();
     }
@@ -59,4 +67,48 @@ public class HomeActivity extends AppCompatActivity {
         menuItem.setChecked(true);
     }
 
+    /*
+    -------------------------------------------Firebase-----------------------------------------------
+    */
+
+    private void checkCurrentUser(FirebaseUser user){
+        Log.d(TAG, "checkCurrentUser: checking if user is logged in");
+
+        if(user == null){
+            Intent intent = new Intent(mContext, LoginActivity.class);
+            startActivity(intent);
+        }
+    }
+
+    private void setupFirevaseAuth(){
+        Log.d(TAG, "setupFirebaseAuth: setting up firebase auth. ");
+
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+
+                checkCurrentUser(user);
+                if(user != null){
+                    Log.d(TAG, "onAuthStateChange: signed_in:" + user.getUid());
+                }else{
+                    Log.d(TAG, "onAuthStateChanged: signed_out");
+                }
+            }
+        };
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    public void onStop(){
+        super.onStop();
+        if(mAuthListener != null){
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
 }
